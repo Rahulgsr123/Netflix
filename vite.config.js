cc09695befca6c41a53044b1fc026d518b3c3af7
Rahulgsr123 <rahulgsr59@gmail.com>
2025-07-14 12:41:29 +0530

    msg

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Netflix/',
+     build: {
+    chunkSizeWarningLimit: 1000, // Increase limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split vendor chunks
+          }
+        }
+      }
+    }
+  }
 })
