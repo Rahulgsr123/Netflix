6857a5ab7ffaf8a7f2f25950a033c028204e78c7
vasanthkvn <katravathvasanth@gmail.com>
2025-08-19 21:54:01 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,14 @@ export default defineConfig({
   plugins: [react()],
   base: '/Netflix/',
      build: {
+          outDir: 'dist',
+    assetsDir: 'assets',
     chunkSizeWarningLimit: 1000, // Increase limit
     rollupOptions: {
       output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor'; // Split vendor chunks
