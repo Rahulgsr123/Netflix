9ef590cba399e87d89edf6c113e9d6fbcb050985
vasanthkvn <katravathvasanth@gmail.com>
2025-08-20 15:02:19 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix/',
+  base: '/Netflix',
      build: {
           outDir: 'dist',
     assetsDir: 'assets',
